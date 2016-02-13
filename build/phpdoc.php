036170ab6858fbfec02a11c970a8d6185e2447c6
abrahamgreyson <82011220@qq.com>
2016-02-13 22:53:50 +0000

    风格修复。

--- build/phpdoc.php
+++ build/phpdoc.php
@@ -1,6 +1,1 @@
 <?php
-/**
- * @link  : http://www.yinhexi.com/
- * @author: AbrahamGreyson <82011220@qq.com>
- * @date  : 01/02/2016
- */
--- src/Qiniu/QiniuClient.php
+++ src/Qiniu/QiniuClient.php
@@ -1,9 +1,5 @@
 <?php
-/**
- * @link  : http://www.yinhexi.com/
- * @author: AbrahamGreyson <82011220@qq.com>
- * @date  : 01/22/2016
- */
+
 
 namespace CloudStorage\Qiniu;
 
