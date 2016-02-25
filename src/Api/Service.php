d73b66b6fed748372a3a5de23e06be032425e5f7
abrahamgreyson <82011220@qq.com>
2016-02-25 05:04:15 +0000

    API 模型。

--- src/Api/Service.php
+++ src/Api/Service.php
@@ -28,20 +28,59 @@ class Service extends AbstractModel
     {
         static $default = [
             'operations' => [],
-            'shapes' => [],
-            'metadata' => [],
+            'shapes'     => [],
+            'metadata'   => [],
         ];
         static $defaultMeta = [
-            'version' => null,
-            'service' => null,
-            'endpoint'  => null,
-            'signature' => null,
+            'version'          => null,
+            'service'          => null,
+            'endpoint'         => null,
+            'signature'        => null,
             'signatureVersion' => null,
-            'protocol' => null
+            'protocol'         => null,
         ];
 
         $definition += $default;
         $definition['metadata'] += $defaultMeta;
         $this->apiProvider = $provider;
+    }
+
+    /**
+     * 为给定的 API 对象创建请求序列器。
+     *
+     * @param Service $api      包含请求协议的 API。
+     * @param string  $endpoint 发送请求的端点。
+     *
+     * @return callable
+     * @throws \UnexpectedValueException
+     */
+    public static function createSerializer(Service $api, $endpoint)
+    {
+        static $mapping = [
+            'json'      => '',
+            'query'     => '',
+            'rest-json' => '',
+            'rest-xml'  => '',
+        ];
+
+        $protocol = $api->getProtocol();
+
+        if (isset($mapping[$protocol])) {
+            return new $mapping[$protocol]($api, $endpoint);
+        }
+
+        throw new \UnexpectedValueException(
+            'Unknown protocol: ' . $protocol
+        );
+    }
+
+    /**
+     * 获取服务 API 的协议。
+     *
+     * @return string
+     */
+    private function getProtocol()
+    {
+        return $this->definition['metadata']['protocol'];
     }
 }
--- src/Api/data/Upyun/v1/api.php
+++ src/Api/data/Upyun/v1/api.php
@@ -7,4 +7,64 @@
  * @license: MIT
  */
 
-return [];
+return [
+    'version'    => 'v1',
+    'metadata'   => [
+        'apiVersion'          => 'v1',
+        'checksumFormat'      => 'md5',
+        'endpointPrefix'      => 'v0',
+        'globalEndpoint'      => 'api.upyun.com',
+        'protocol'            => 'rest-xml',
+        'serviceAbbreviation' => 'Upyun',
+        'serviceFullName'     => 'Upyun cloud storage and CDN service.',
+        'signatureVersion'    => 'v1',
+        'timestampFormat'     => 'rfc822',
+    ],
+    'operations' => [
+        'CreateBucket' => [
+            'name'   => 'CreateBucket',
+            'http'   => [
+                'method'     => 'PUT',
+                'requestUri' => '/{Bucket}',
+            ],
+            'input'  => [
+                'shape' => 'CreateBucketRequest',
+            ],
+            'output' => [
+                'shape' => 'CreateBucketOutput',
+            ],
+            'errors' => [
+                'shape' => 'BucketAlreadyExists',
+            ],
+            'documentationUrl' => 'https://www.google.com',
+            'alias' => 'PutBucket'
+        ],
+    ],
+    'shapes'     => [
+        'CreateBucketRequest' => [
+            'type' => 'structure',
+            'required' => [
+                'Bucket'
+            ],
+            'members' => [
+                'ACL' => [
+                    'shape'        => 'BucketCannedACL',
+                    'location'     => 'header',
+                    'locationName' => 'x-amz-zcl',
+                ],
+                'Bucket' => [
+                    'shape'        => 'BucketName',
+                    'location'     => 'uri',
+                    'locationName' => 'Bucket',
+                ],
+                'CreateBucketConfiguration' => [
+                    'shape' => 'CreateBucketConfiguration',
+                    'locationName' => 'CreateBucketConfiguration',
+                    'xmlNamespace' => [
+                        'uri' => 'www.google.com'
+                    ]
+                ]
+            ]
+        ]
+    ],
+];
